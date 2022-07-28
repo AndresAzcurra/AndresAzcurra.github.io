dd6ac86cb534ec48da746985b18dba533e8600d7
andres <andresazcurra@gmail.com>
2022-07-28 05:51:22 -0300

    se modifico en la clase 13  el login de la clase 12 y se agrego un archivo js

---
+++ clase12/javascript.js
@@ -0,0 +1,9 @@
+  
+  function mostrarValores(){  
+let usuario =document.getElementById("username").value;
+let password =document.getElementById('password').value;
+let userAndpass =usuario + '-'+ password;
+
+alert(userAndpass);
+
+}
